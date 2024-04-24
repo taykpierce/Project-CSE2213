1b1b5be2d87a2cc49283776f49e91eb219f51bde
Andre Dillon <149001769+AADD03@users.noreply.github.com>
2024-04-24 15:38:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -61,6 +61,39 @@ def mainMenu(user, cart, inventory, history):
             user.logout()
 
             print("Successful logout.")
+            break 
+
+        elif option == "1":
+            user.viewAccountInformation()
+        
+        elif option == "2":
+            print ("Inventory Menu:")
+            print ("a. View Inventory")
+            print ("b. Search Inventory")
+            inventory_option = input("Enter your inventory choice: ")
+
+            if inventory_option == "a":
+                inventory.viewInventory()
+            elif inventory_option == "b":
+                title = input("Enter the title to search: ")
+                inventory.searchInventory(title)
+            
+            else:
+                print("That not a valid")
+
+        elif option == "3":
+            print("Cart Menu:")
+            print("a. View Cart")
+            print("b. Add Item to Cart")
+            print("c. Remove Item from Cart")
+            cart_option = input("Enter your cart choice: ")
+
+            if cart_option == "a":
+                cart.viewCart(user.getUserID())
+            
+            elif cart_option == "b":
+                orderID = 
+
 
         ## incorrect menu option
         else:
